a6b3bf9aee3040b5945a134a28094e2df0f68c49
evlev777 <levkovich.evgeniy.98@gmail.com>
2024-03-21 16:50:07 +0300

    add items on database

--- main/library/urls.py
+++ main/library/urls.py
@@ -5,5 +5,6 @@ from .views import IndexView, BookListView
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('catalog/', BookListView.as_view(), name='catalog'),
+    path('page/<int:page>/', BookListView.as_view(), name='paginator'),
     path('author/<int:author_id>/', BookListView.as_view(), name='author')
 ]
